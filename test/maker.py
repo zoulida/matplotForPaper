8d698e68a0d6a9c35ddf491cd3ad06f85d6f9f9d
Administrator <zoulida@163.com>
2019-04-09 10:02:00 +0800

    Initial commit

---
+++ test/maker.py
@@ -0,0 +1,10 @@
+__author__ = 'zoulida'
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2 * np.pi, 10)
+y1, y2 = np.sin(x), np.cos(x)
+
+plt.plot(x, y1, marker='o', mec='r', mfc='w')
+plt.plot(x, y2, marker='*', ms=10)
+plt.show()
